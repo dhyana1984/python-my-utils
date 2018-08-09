f9b6b7a91cf966ea780fb0b694fea053f455b634
Chris <chris.yee@qq.com>
2018-08-10 00:37:43 +0800

    私有属性装饰器
    在装饰器中声明私有属性

---
+++ privatePropertyDecorate.py
@@ -0,0 +1,28 @@
+'''
+实现私有属性的装饰器
+'''
+
+traceMe = False
+def trace(*args):
+    if traceMe: print('[' + ' '.join(map(str, args)) + ']')
+def Private(*privates): 
+    def onDecorator(aClass): 
+        class onInstance: 
+            def __init__(self, *args, **kargs):
+                self.wrapped = aClass(*args, **kargs)
+            def __getattr__(self, attr): 
+                trace('get:', attr) 
+                if attr in privates:
+                    raise TypeError('private attribute fetch: ' + attr)
+                else:
+                    return getattr(self.wrapped, attr)
+            def __setattr__(self, attr, value): 
+                trace('set:', attr, value) 
+                if attr == 'wrapped': 
+                    self.__dict__[attr] = value 
+                elif attr in privates:
+                    raise TypeError('private attribute change: ' + attr)
+                else:
+                    setattr(self.wrapped, attr, value) 
+        return onInstance 
+    return onDecorator
